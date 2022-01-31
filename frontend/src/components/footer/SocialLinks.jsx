a123b4c9b3864abe4761bb46aa85d6336f12e36a
Alexander  Roaas <alexapr@stud.ntnu.no>
2022-01-31 15:25:55 +0000

    Fixes to the SoMe link

--- frontend/src/components/footer/SocialLinks.jsx
+++ frontend/src/components/footer/SocialLinks.jsx
@@ -2,43 +2,43 @@ import './SocialLinks.css';
 
 const SocialLinks = () => {
   return (
-    <div className="SocialLinks">
-      <a className="SocialIcon" href="https://www.linkedin.com/company/linjeforeningen-login/about">
+    <div className="SocialLinks" target="_blank" rel="noreferrer">
+      <a className="SocialIcon" href="https://www.linkedin.com/company/linjeforeningen-login/about" target="_blank" rel="noreferrer">
         <picture>
           <source srcSet={process.env.PUBLIC_URL + '/icons/Linkedin_logo.svg'} />
           <img alt="Linkedin's logo" />
         </picture>
       </a>
       <div className="SocialSpacer" />
-      <a className="SocialIcon" href="https://facebook.com/LogNTNU">
+      <a className="SocialIcon" href="https://facebook.com/LogNTNU" target="_blank" rel="noreferrer">
         <picture>
           <source srcSet={process.env.PUBLIC_URL + '/icons/Facebook_logo.svg'} />
           <img alt="Facebook's logo" />
         </picture>
       </a>
       <div className="SocialSpacer" />
-      <a className="SocialIcon" href="https://www.instagram.com/login_linjeforening/">
+      <a className="SocialIcon" href="https://www.instagram.com/login_linjeforening/" target="_blank" rel="noreferrer">
         <picture>
           <source srcSet={process.env.PUBLIC_URL + '/icons/Instagram_logo.svg'} />
           <img alt="Instagram's logo" />
         </picture>
       </a>
       <div className="SocialSpacer" />
-      <a className="SocialIcon" href="https://git.logntnu.no/">
+      <a className="SocialIcon" href="https://git.logntnu.no/" target="_blank" rel="noreferrer">
         <picture>
           <source srcSet={process.env.PUBLIC_URL + '/icons/Gitlab_logo.svg'} />
           <img alt="GitLab's logo" />
         </picture>
       </a>
       <div className="SocialSpacer" />
-      <a className="SocialIcon" href="https://redmine.logntnu.no/">
+      <a className="SocialIcon" href="https://redmine.logntnu.no/" target="_blank" rel="noreferrer">
         <picture>
           <source srcSet={process.env.PUBLIC_URL + '/icons/Redmine_logo.svg'} />
           <img alt="Redmine's logo" />
         </picture>
       </a>
       <div className="SocialSpacer" />
-      <a className="SocialIcon" href="https://discord.gg/login-ntnu">
+      <a className="SocialIcon" href="https://discord.gg/login-ntnu" target="_blank" rel="noreferrer">
         <picture>
           <source srcSet={process.env.PUBLIC_URL + '/icons/Discord_logo.svg'} />
           <img alt="Discord's logo" />
