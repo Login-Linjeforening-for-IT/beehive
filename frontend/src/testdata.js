d5050716a80a5adf7276bac29c46e0336c3d696f
Alexander  Roaas <alexapr@stud.ntnu.no>
2022-02-28 18:54:01 +0000

    Event containers

---
+++ frontend/src/testdata.js
@@ -0,0 +1,18 @@
+export const eventData = [
+    {
+        eventName: "Bakedag",
+        eventDesc: "Dette er en dag for baking",
+        eventDate: "2022-01-01",
+        eventTime: "10:00:00",
+        eventLocation: "A255",
+        eventLink: null
+    },
+    {
+        eventName: "Sykkeltur",
+        eventDesc: "Sykkeltur til gokk",
+        eventDate: "2022-02-02",
+        eventTime: "13:00:00",
+        eventLocation: "A256",
+        eventLink: null
+    }
+]
