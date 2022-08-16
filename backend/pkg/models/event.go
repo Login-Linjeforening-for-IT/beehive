b4e30a239893cc236f0b3c8b4697cab51be1528d
Alexander Roaas <alexapr@stud.ntnu.no>
2022-08-16 13:20:50 +0200

    Fix string in model to support Null how we want to

--- backend/pkg/models/event.go
+++ backend/pkg/models/event.go
@@ -4,56 +4,55 @@ Denne har alle structen for event api
 package models
 
 import "time"
-import "database/sql"
 
 // Model for the preview of an event (listing)
 type EventPreview struct {
 	EventID			int 			`json:"eventID"`
 	Parent			int 			`json:"parent"`
-	Organizer   sql.NullString 		`json:"organizer"`
-	Name        sql.NullString 		`json:"eventname"`
+	Organizer   *string 		`json:"organizer"`
+	Name        *string 		`json:"eventname"`
 	StartT      time.Time `json:"startt"`
-	Audience    sql.NullString 		`json:"audience"`
-	Category    sql.NullString 		`json:"category"`
-	Image       sql.NullString 		`json:"image"`
-	FbLink      sql.NullString 		`json:"fblink"`
-	DiscordLink sql.NullString 		`json:"discordlink"`
-	RoomNo				sql.NullString    `json:"roomno"`
-	Campus 				sql.NullString 		`json:"campus"`
-	Street 				sql.NullString 		`json:"street"`
-	Postcode 			sql.NullString		`json:"postcode"`
-	City 					sql.NullString 		`json:"city"`
+	Audience    *string 		`json:"audience"`
+	Category    *string 		`json:"category"`
+	Image       *string 		`json:"image"`
+	FbLink      *string 		`json:"fblink"`
+	DiscordLink *string 		`json:"discordlink"`
+	RoomNo				*string    `json:"roomno"`
+	Campus 				*string 		`json:"campus"`
+	Street 				*string 		`json:"street"`
+	Postcode 			*string		`json:"postcode"`
+	City 					*string 		`json:"city"`
 }
 
 // Model for a single event
 type Event struct {
 	EventID 			int 			`json:"eventID"`
 	Parent 				int 			`json:"parent"`
-	Organizer 		sql.NullString 		`json:"organizer"`
-	Name 					sql.NullString 		`json:"eventname"`
+	Organizer 		*string 		`json:"organizer"`
+	Name 					*string 		`json:"eventname"`
 	StartT				time.Time	`json:"startt"`
 	EndT 					time.Time `json:"endt"`
 	PublishT 			time.Time `json:"publisht"`
-	Description 	sql.NullString 		`json:"description"`
-	Audience 			sql.NullString 		`json:"audience"`
-	Category 			sql.NullString  	`json:"category"`
-	Image 				sql.NullString 		`json:"image"`
-	FBLink 				sql.NullString		`json:"fblink"`
-	DiscordLink 	sql.NullString 		`json:"discordlink"`	
-	OrganizerLogo sql.NullString    `json:"organizerlogo"`
-	OrganizerLink sql.NullString    `json:"organizerlink"`
-	RoomNo				sql.NullString    `json:"roomno"`
-	Campus 				sql.NullString 		`json:"campus"`
-	MazeRef 			sql.NullString 		`json:"mazeref"`
-	Street 				sql.NullString 		`json:"street"`
-	Postcode 			sql.NullString		`json:"postcode"`
-	City 					sql.NullString 		`json:"city"`
+	Description 	*string 		`json:"description"`
+	Audience 			*string 		`json:"audience"`
+	Category 			*string  	`json:"category"`
+	Image 				*string 		`json:"image"`
+	FBLink 				*string		`json:"fblink"`
+	DiscordLink 	*string 		`json:"discordlink"`	
+	OrganizerLogo *string    `json:"organizerlogo"`
+	OrganizerLink *string    `json:"organizerlink"`
+	RoomNo				*string    `json:"roomno"`
+	Campus 				*string 		`json:"campus"`
+	MazeRef 			*string 		`json:"mazeref"`
+	Street 				*string 		`json:"street"`
+	Postcode 			*string		`json:"postcode"`
+	City 					*string 		`json:"city"`
 }
 
 
 // Model for category
 type Category struct {
-	Name				sql.NullString 	`json:"Name"`
-	Color				sql.NullString 	`json:"Color"`
-	Description	sql.NullString 	`json:"Description"`
+	Name				*string 	`json:"Name"`
+	Color				*string 	`json:"Color"`
+	Description	*string 	`json:"Description"`
 }
