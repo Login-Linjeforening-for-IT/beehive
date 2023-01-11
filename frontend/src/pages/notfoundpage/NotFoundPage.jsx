ef5e4dbd71404d10adad09dcb430a8e5bdbdd30e
Simon Edna <simonedna02@gmail.com>
2023-01-11 14:46:08 +0000

    404 pizza

--- frontend/src/pages/notfoundpage/NotFoundPage.jsx
+++ frontend/src/pages/notfoundpage/NotFoundPage.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {withTranslation} from "react-i18next";
+import { config } from '../../Constants';
+import './NotFoundPage.css';
 
 const NotFoundPage = ({t}) => {
 	return (
-		<div>
-            <br/>
-            <br/>
-            <br/>
-            <h1 style={{textAlign:"center"}}>404</h1>
+		<div className="NotFound">
+            <h1>{t('header-1')}</h1>
+            <picture className="Picture-Pizza">
+              <source srcSet={config.url.CDN_URL + '/img/pizza404.png'} />
+              <img alt='Hangry 404'/>
+            </picture>
+            <h1>{t('header-2')}</h1>
+            <br></br>
             <p style={{textAlign:"center"}}>
               {t('msg')}
-                <br/>
-                <br/>
-                <br/>
+                <br></br>
                 <Link to="/">{t('help')}</Link>
             </p>
         </div>
