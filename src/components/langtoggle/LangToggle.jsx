0076dc67457d6402063905af240d4efe66d69374
Johannes Aas <johhaa@stud.ntnu.no>
2023-03-07 20:23:34 +0000

    v0.1.0

--- src/components/langtoggle/LangToggle.jsx
+++ src/components/langtoggle/LangToggle.jsx
@@ -1,27 +1,31 @@
-import {withTranslation} from "react-i18next";
-import {useEffect, useState} from 'react';
+import {withTranslation} from 'react-i18next'
+import {useEffect, useState} from 'react'
 
 import './LangToggle.css'
 
 const LangToggle = ({i18n}) => {
-  const [buttonText, setButtonText] = useState('');
+  const [buttonText, setButtonText] = useState('')
+  const [jump, setJump] = useState(false);
   
   useEffect( () => {
-    i18n.language === 'no' || i18n.language === 'nb' ? setButtonText('NO') : setButtonText('EN')
-  }, [i18n.language]);
+    i18n.language === 'no' || i18n.language === 'nb' ? setButtonText('en') : setButtonText('no')
+  }, [i18n.language])
 
   function handleClick(event) {
     if (i18n.language === 'no' || i18n.language === 'nb') {
-      i18n.changeLanguage(event.target.value = 'en');
-      setButtonText('NO');
+      i18n.changeLanguage(event.target.value = 'en')
+      setButtonText('en')
     } else {
-      i18n.changeLanguage(event.target.value = 'nb');
-      setButtonText('EN');
+      i18n.changeLanguage(event.target.value = 'nb')
+      setButtonText('no')
     }
+
+    setJump(true)
+    setTimeout(() => setJump(false), 400)
   }
 
   return(
-    <div value={i18n.language} onClick={handleClick} className="lang-toggle">{buttonText}</div>
+    <div value={i18n.language} onClick={handleClick} className='lang-toggle'><i className={`lang-toggle__icon material-symbols-sharp ${jump ? 'lang-toggle__icon--jump' : ''}`}>language</i> {buttonText}</div>
   )
 }
 
