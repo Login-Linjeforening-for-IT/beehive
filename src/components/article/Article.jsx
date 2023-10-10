f8345435db165310a7bdcd8c79bd2b3580da55a5
gudleik <johhaa@stud.ntnu.no>
2023-10-10 04:57:44 +0200

    Article component

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import * as TimeFormatter from "../../utils/DatetimeFormatter";
+import { withTranslation } from "react-i18next";
+import "./Article.css";
+
+
+const Article = ({ i18n, title, publishTime, informational, description }) => {
+
+    const useEng = i18n.language === "en";
+
+    return (
+        <div className="article">
+            <h1 className="article__header">{title}</h1>
+            <p className="article__published">
+              {useEng ? "PUBLISHED: " : "PUBLISERT: "}
+              {/*TimeFormatter.formatDateTDY(
+                new Date(publishTime),
+                useEng ? "en" : "no"
+              )*/}
+              {TimeFormatter.formatPublishedTime(publishTime, useEng ? "en" : "no")}
+            </p>
+            {informational && (
+              <div className="article__informational">
+                <i className="article__informational-icon material-symbols-sharp">
+                  info
+                </i>
+                <div className="article__informational-msg">
+                  {informational}
+                </div>
+              </div>
+            )}
+            <article
+                className="article__main"
+                dangerouslySetInnerHTML={{ __html: description }}
+            />
+        </div>
+    );
+};
+
+export default withTranslation()(Article);
