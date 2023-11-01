3f55b1c2e87c345afffa644a365995ffe77c4f85
gudleik <johhaa@stud.ntnu.no>
2023-11-01 16:29:40 +0100

    filter translation

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState, useEffect } from "react";
+import { withTranslation } from "react-i18next";
 import './filter.css';
 
 const getFilterGroupItemOnSelectWithin = (onSelect, filterGroupItemID) => {
@@ -7,9 +8,73 @@ const getFilterGroupItemOnSelectWithin = (onSelect, filterGroupItemID) => {
     }
 }
 
-export const FilterGroup = ({ filters, onApply }) => {
+const getFilterItemOnSelect = (onSelect, filterID) => {
+    return (isSelected) => {
+        onSelect(filterID, isSelected);
+    }
+}
+
+const Filter = withTranslation()(({ i18n, label, filter, showCount, onSelect, type, resetTrigger }) => {
+
+    const lang = i18n.language === "en" ? "en" : "no";
+
+    return (
+        <div className="filter">
+            <div className="filter__title">{ label[lang] }</div>
+            <div className={`filter__items filter__items--${type}`}>
+                {filter.map((filterItem) => {
+                    return (
+                        <FilterItem
+                            key={filterItem.id}
+                            filter={filterItem}
+                            type={type}
+                            showCount={showCount}
+                            onSelect={getFilterItemOnSelect(onSelect, filterItem.id)}
+                            resetTrigger={resetTrigger}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+})
+
+const FilterItem = withTranslation()(({ i18n, filter, showCount, onSelect, resetTrigger, type }) => {
+
+    const lang = i18n.language === "en" ? "en" : "no";
+
+    const [ checked, setChecked ] = useState(false);
+
+    useEffect(() => {
+        setChecked(false);
+    }, [resetTrigger]);
+
+    const select = () => {
+        setChecked(!checked);
+        onSelect(!checked);
+    }
+
+    return (
+        <label className={`filter__item filter__item--${type}`} htmlFor={filter.label[lang]}>
+            <input className={`filter__input filter__input--${type}`}
+                id={filter.label[lang]}
+                type='checkbox'
+                onChange={e => select(e.target.checked)}
+                checked={checked}
+            />
+            {type == "check" &&
+                <div className="filter__checkbox"></div>
+            }
+            <div className={`filter__item-name filter__item-name--${type}`}>{filter.label[lang]}{ showCount && ' (' + filter.count + ')' }</div>
+        </label>
+    );
+});
+
+const FilterGroup = ({ t, filters, onApply }) => {
     const selectedFilters = useRef({});
     const [ resetTrigger, setResetTrigger ] = useState(false);
+
+    // const lang = i18n.language == "en" ? "en" : "no";
 
     const onReset = () => {
         selectedFilters.current = {};
@@ -46,10 +111,12 @@ export const FilterGroup = ({ filters, onApply }) => {
         <div className="filter-groups">
             {
                 Object.entries(filters).map(([filterGroupItemID, filterGroupItem]) => {
+                    console.log('filterGroupItem', filterGroupItem)
                     return (
                         <Filter
                             key={filterGroupItemID}
                             label={filterGroupItem.label}
+                            showCount={filterGroupItem.showCount}
                             filter={Object.values(filterGroupItem.filters)}
                             type={filterGroupItem.type}
                             onSelect={getFilterGroupItemOnSelectWithin(onSelect, filterGroupItemID)}
@@ -61,65 +128,10 @@ export const FilterGroup = ({ filters, onApply }) => {
             <div
                 className="filter-groups__reset standard-button standard-button--discret"
                 onClick={onReset}>
-                Reset <i className='material-symbols-sharp'>replay</i>
+                {t('reset')} <i className='material-symbols-sharp'>replay</i>
             </div>
         </div>
     );
 }
 
-const getFilterItemOnSelect = (onSelect, filterID) => {
-    return (isSelected) => {
-        onSelect(filterID, isSelected);
-    }
-}
-
-const Filter = ({ label, filter, onSelect, type, resetTrigger }) => {
-
-    return (
-        <div className="filter">
-            <div className="filter__title">{ label }</div>
-            <div className={`filter__items filter__items--${type}`}>
-                {filter.map((filterItem) => {
-                    return (
-                        <FilterItem
-                            key={filterItem.id}
-                            filter={filterItem}
-                            type={type}
-                            onSelect={getFilterItemOnSelect(onSelect, filterItem.id)}
-                            resetTrigger={resetTrigger}
-                        />
-                    );
-                })}
-            </div>
-        </div>
-    );
-}
-
-const FilterItem = ({ filter, onSelect, resetTrigger, type }) => {
-
-    const [ checked, setChecked ] = useState(false);
-
-    useEffect(() => {
-        setChecked(false);
-    }, [resetTrigger]);
-
-    const select = () => {
-        setChecked(!checked);
-        onSelect(!checked);
-    }
-
-    return (
-        <label className={`filter__item filter__item--${type}`} htmlFor={filter.label}>
-            <input className={`filter__input filter__input--${type}`}
-                id={filter.label}
-                type='checkbox'
-                onChange={e => select(e.target.checked)}
-                checked={checked}
-            />
-            {type == "check" &&
-                <div className="filter__checkbox"></div>
-            }
-            <div className={`filter__item-name filter__item-name--${type}`}>{filter.label}</div>
-        </label>
-    );
-}
+export default withTranslation("filter")(FilterGroup);
