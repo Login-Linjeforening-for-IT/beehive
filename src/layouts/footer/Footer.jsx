7ec7778b0156d87725dc6b9146368d372f9b4a7d
Johannes Aas <johhaa@stud.ntnu.no>
2024-05-01 21:55:54 +0000

    Resolve "Add contact info (mail, address) in footer"

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -12,22 +12,55 @@ const Footer = ({ t }) => {
 
   return (
     <div className='footer'>
-      <div className='footer__flex-container'>
-        <picture className='footer__picture footer__picture-login'>
-          <source srcSet={process.env.PUBLIC_URL + '/img/logo/logo-tekst-white.svg'} />
-          <img alt='Login - Linjeforeningen for IT' />
-        </picture>
-        <a href='https://www.mnemonic.io/' target='_blank' className='footer__picture footer__picture-sponsor'>
-          <picture>
-            <source srcSet={config.url.CDN_URL + '/img/company/mnemonic-logo_light-nopayoff-2021.svg '} />
-            <img alt='mnemonic' />
+      <div className='footer-logos'>
+        <div className='footer-logos__item'>
+          <picture className='footer-logos__item-picture'>
+            <source srcSet={process.env.PUBLIC_URL + '/img/logo/logo-tekst-white.svg'} />
+            <img className='footer-logos__item-image' alt='Login - Linjeforeningen for IT' />
           </picture>
-          <p>{t('footer.sponsor')}</p>
-        </a>
+        </div>
+        <div className='footer-logos__item'>
+          <a href='https://www.mnemonic.io/' target='_blank'>
+            <picture className='footer-logos__item-picture'>
+              <source srcSet={config.url.CDN_URL + '/img/company/mnemonic-logo_light-nopayoff-2021.svg '} />
+              <img className='footer-logos__item-image' alt='mnemonic' />
+            </picture>
+          </a>
+          <p className='footer-logos__item-text'>{t('footer.sponsor')}</p>
+        </div>
+      </div>
+      <div className='footer-contact-info'>
+        <div className='footer-contact-info__section'>
+          <h4 className='footer-contact-info__header'>{t('footer.contactInfo.address.header')}</h4>
+          <p className='footer-contact-info__text'>
+            {t('footer.contactInfo.address.info1')}
+            <br/>
+            {t('footer.contactInfo.address.info2')}
+            <br/>
+            {t('footer.contactInfo.address.info3')}
+          </p>
+        </div>
+        <div className='footer-contact-info__section'>
+          <h4 className='footer-contact-info__header'>{t('footer.contactInfo.email')}</h4>
+          <p className='footer-contact-info__text'>
+            <a className='footer-contact-info__link standard-link--underscore-hover' href='mailto:kontakt@login.no'>kontakt@login.no</a>
+          </p>
+        </div>
       </div>
       <SocialLinks />
-      <p className='footer__p' dangerouslySetInnerHTML={{ __html: ` ${t('footer.copy1')} ${currentYear} ${t('footer.copy2')}` }} />
-      {version!=="undefined" ? <p className='footer__p'>{version}</p> : null}
+      <div className='footer-bottom'>
+        <p className='footer-bottom__text' dangerouslySetInnerHTML={{ __html: ` ${t('footer.copy1')} ${currentYear} ${t('footer.copy2')}` }} />
+        {typeof version !== 'undefined' ?
+          <a 
+            className='footer-bottom__version-tag'
+            target='_blank'
+            href={'https://git.logntnu.no/tekkom/web/beehive/frontend/-/tags/' + version} 
+          >
+            v{version}
+          </a>
+          : null
+        }
+      </div>
     </div>
   );
 }
