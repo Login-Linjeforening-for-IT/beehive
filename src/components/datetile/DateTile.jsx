2e8e8080703fdece999f0c8637343695b9030df8
johannes-aas <johhaa@stud.ntnu.no>
2024-08-12 04:21:09 +0200

    added features to date tile component

--- src/components/datetile/DateTile.jsx
+++ src/components/datetile/DateTile.jsx
@@ -1,8 +1,9 @@
 import { withTranslation } from "react-i18next";
+import * as ColorManipulation from "../../utils/ColorManipulation.js";
 import './DateTile.css'
 
 
-const DateTile = ({ i18n, startDate, endDate, color }) => {
+const DateTile = ({ i18n, startDate, endDate, color, varient='regular', useDayText=false }) => {
     const sTime = new Date(startDate);
     const eTime = new Date(endDate);
 
@@ -19,26 +20,52 @@ const DateTile = ({ i18n, startDate, endDate, color }) => {
         no: ['jan', 'feb', 'mar', 'apr', 'mai', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'des']
     };
 
-    if (sDate === eDate) {
+    const daysOfWeek = {
+        en: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        no: ['Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør']
+    };
+
+    let background;
+
+    if (ColorManipulation.isValidHex(color)) {
+        if (varient === 'regular') {
+            background = ColorManipulation.createGradient(color, 1);
+        } else {
+            background = ColorManipulation.hexToRgba(color, 0.7);
+        }
+    } else {
+        background = color;
+    }
+
+    if (useDayText) {
         return (
-            <div className='date-tile' style={{background: color}}>
+            <div className={`date-tile date-tile--${varient} ${sDate === eDate ? '' : 'date-tile--wide'}`} style={{background: background}}>
                 <div className='date-tile__date'>
-                    <div className='date-tile__day'>{sDate}</div>
+                    <div className='date-tile__dayofweek'>{daysOfWeek[lang][sTime.getDay()]}.</div>
+                </div>
+            </div>
+        );
+    }
+    if (sMonth === eMonth) {
+        return (
+            <div className={`date-tile date-tile--${varient} ${sDate === eDate ? '' : 'date-tile--wide'}`} style={{background: background}}>
+                <div className='date-tile__date'>
+                    <div className={`date-tile__day ${sDate === eDate ? '' : 'date-tile__day--wide'}`}>{(sDate === eDate) ? sDate : sDate + '-' + eDate}</div>
                     <div className='date-tile__month'>{months[lang][eMonth]}</div>
                 </div>
             </div>
         );
     } else {
         return (
-            <div className='date-tile' style={{background: color}}>
+            <div className={`date-tile date-tile--wide date-tile--${varient}`} style={{background: background}}>
                 <div className='date-tile__date'>
-                    <div className='date-tile__day'>{sDate}</div>
-                    <div className='date-tile__month'>{months[lang][sMonth]}</div>
+                    <div className='date-tile__day date-tile__day--wide'>{sDate}</div>
+                    <div className='date-tile__month date-tile__month--wide'>{months[lang][sMonth]}</div>
                 </div>
                 <div className='date-tile__devider'>-</div>
                 <div className='date-tile__date'>
-                    <div className='date-tile__day'>{eDate}</div>
-                    <div className='date-tile__month'>{months[lang][eMonth]}</div>
+                    <div className='date-tile__day date-tile__day--wide'>{eDate}</div>
+                    <div className='date-tile__month date-tile__month--wide'>{months[lang][eMonth]}</div>
                 </div>
             </div>
         );
