14357310553e3a4fa78aa6f73d729afab2024161
Martin <martcl@stud.ntnu.no>
2024-08-27 22:31:24 +0000

    refactor: using propper react to switch themes

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,91 +2,96 @@ import { config } from "../Constants";
 
 const baseUrl = config.url.API_URL;
 
-const _apiFetch = async (path) => {
-    try {
-        const response = await fetch(baseUrl + path, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                // 'mode': 'no-cors',
-            },
-        });
-        const data = await response.json();
-        return response.ok ? [data, null] : [null, data];
-    } catch (error) {
-        return [null, error];
-    }
-}
 
-const _arrayToQueryString = (arr) => {
-    return arr.join(',');
-}
+const _fetchWrapper = async (path, options = {}) => {
+    const defaultOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    const finalOptions = { ...defaultOptions, ...options };
+
+    try {
+        const response = await fetch(baseUrl + path, finalOptions);
+        const data = await response.json();
+
+        if (!response.ok) {
+            return [null, data];
+        }
+
+        return [data, null];
+    } catch (error) {
+        return [null, error.message || 'Unknown error! Please contact TekKom'];
+    }
+};
 
 const getJob = async (jobID) => {
     const path = `/jobs/${jobID}`;
-    return await _apiFetch(path);
+    return await _fetchWrapper(path);
 }
 
 const getJobRow = async (jobID) => {
     const path = `/jobs/${jobID}/row`;
-    return await _apiFetch(path);
+    return await _fetchWrapper(path);
 }
 
 const getJobs = async (skills = null, cities = null, organizations = null, jobtypes = null, limit = 20, offset = 0) => {
-    const queryParts = { limit, offset };
-    skills && (queryParts.skills = _arrayToQueryString(skills));
-    cities && (queryParts.cities = _arrayToQueryString(cities));
-    organizations && (queryParts.organizations = _arrayToQueryString(organizations));
-    jobtypes && (queryParts.jobtypes = _arrayToQueryString(jobtypes));
+    const queryParts = new URLSearchParams({ limit, offset });
 
-    const query = new URLSearchParams(queryParts);
-    const path = `/jobs/?${query}`;
-    return await _apiFetch(path);
+    if (skills) queryParts.append('skills', skills.join(','));
+    if (cities) queryParts.append('cities', cities.join(','));
+    if (organizations) queryParts.append('organizations', organizations.join(','));
+    if (jobtypes) queryParts.append('jobtypes', jobtypes.join(','));
+
+    const path = `/jobs/?${queryParts.toString()}`;
+    return await _fetchWrapper(path);
 }
 
 const getEvent = async (eventID) => {
     const path = `/events/${eventID}`;
-    return await _apiFetch(path);
+    return await _fetchWrapper(path);
 }
 
 const getEventRow = async (eventID) => {
     const path = `/events/${eventID}/row`;
-    return await _apiFetch(path);
+    return await _fetchWrapper(path);
 }
 
 const getEvents = async (categories = null, limit = 20, offset = 0, highlighted=false) => {
-    const queryParts = { limit, offset, highlighted };
-    categories && (queryParts.categories = _arrayToQueryString(categories));
+    const queryParams = new URLSearchParams({ limit, offset });
 
-    const query = new URLSearchParams(queryParts);
-    const path = `/events/?${query}`;
-    return await _apiFetch(path);
+    if (categories) queryParams.append('categories', categories.join(','));
+    if (highlighted) queryParams.append('highlighted', highlighted);
+
+    const path = `/events/?${queryParams.toString()}`;
+    return await _fetchWrapper(path);
 }
 
 
 const getEventCategoryFilters = async () => {
     const path = `/filters/events/categories`;
-    return await _apiFetch(path);
+    return await _fetchWrapper(path);
 }
 
 const getJobJobtypeFilters = async () => {
     const path = `/filters/jobs/jobtypes`;
-    return await _apiFetch(path);
+    return await _fetchWrapper(path);
 }
 
 const getJobSkillFilters = async () => {
     const path = `/filters/jobs/skills`;
-    return await _apiFetch(path);
+    return await _fetchWrapper(path);
 }
 
 const getJobCityFilters = async () => {
     const path = `/filters/jobs/cities`;
-    return await _apiFetch(path);
+    return await _fetchWrapper(path);
 }
 
 const getJobOrganizationFilters = async () => {
     const path = `/filters/jobs/organizations`;
-    return await _apiFetch(path);
+    return await _fetchWrapper(path);
 }
 
 export {
