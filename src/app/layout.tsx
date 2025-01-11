0c223a9815b5741e83726af830837baa6eeceddd
hermanpe <hermanpe@stud.ntnu.no>
2025-01-11 21:07:31 +0100

    colors and theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 import TopBar from '@components/shared/topbar/TopBar'
 import Footer from '@components/shared/footer/Footer'
 import './globals.css'
+import '../styles/colors.css'
+import '../styles/themes.css'
 
 export const metadata: Metadata = {
     title: 'Login',
