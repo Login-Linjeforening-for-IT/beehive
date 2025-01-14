b956c1d7a0b809892cf52129df6bac930c22d71f
eirikhanasand <eirik.hanasand@gmail.com>
2025-01-14 19:49:13 +0100

    wip 3

--- src/context/langContext.tsx
+++ src/context/langContext.tsx
@@ -1,22 +1,22 @@
-// import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
-// export const LANGUAGES = {
-//     NO: 'no',
-//     EN: 'en',
-// }
+export const LANGUAGES = {
+    NO: 'no',
+    EN: 'en',
+}
 
-// export default function LangContext() {
-//     const [lang, setLang] = useState(LANGUAGES.NO)
+export default function LangContext() {
+    const [lang, setLang] = useState(LANGUAGES.NO)
 
-//     useEffect(() => {
-//         document.body.classList.remove(...Object.values(LANGUAGES))
-//         document.body.classList.add(lang)
-//         localStorage.setItem('lang', lang)
-//     }, [lang])
+    useEffect(() => {
+        document.body.classList.remove(...Object.values(LANGUAGES))
+        document.body.classList.add(lang)
+        localStorage.setItem('lang', lang)
+    }, [lang])
 
-//     function switchLang() {
-//         setLang((prev) => (prev === LANGUAGES.NO ? LANGUAGES.EN : LANGUAGES.NO))
-//     }
+    function switchLang() {
+        setLang((prev) => (prev === LANGUAGES.NO ? LANGUAGES.EN : LANGUAGES.NO))
+    }
 
-//     return { lang, switchLang }
-// }
+    return { lang, switchLang }
+}
