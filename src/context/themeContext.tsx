3f55d60ac079ca8a3633fa975c7baeeb9ae01dea
eirikhanasand <eirik.hanasand@gmail.com>
2025-01-14 21:02:04 +0100

    Working on theme

---
+++ src/context/themeContext.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { createContext, useState, useEffect } from "react"
+
+const COLOR_THEMES = {
+    LIGHT: "light",
+    DARK: "dark",
+}
+
+// const storedTheme = localStorage?.getItem("theme") || COLOR_THEMES.DARK
+
+const ThemeContext = createContext({
+    theme: COLOR_THEMES.DARK,
+    switchTheme: () => {}
+})
+
+export function ThemeProvider({ children }: any) {
+    const [theme, setTheme] = useState(COLOR_THEMES.DARK)
+
+    useEffect(() => {
+        document.body.classList.remove(...Object.values(COLOR_THEMES))
+        document.body.classList.add(theme)
+        localStorage.setItem("theme", theme)
+    }, [theme])
+
+    function switchTheme() {
+        const newTheme = theme === COLOR_THEMES.DARK ? COLOR_THEMES.LIGHT : COLOR_THEMES.DARK
+        setTheme(newTheme)
+    }
+
+    return (
+        <ThemeContext.Provider value={{ theme, switchTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeContext
