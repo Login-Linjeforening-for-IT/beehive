d7599c621aeeae81325e5d714affa56e2ced4149
404-vansh <vansh@vaudit.com>
2025-09-26 11:23:15 +0530

    fix: correct language display in Beehive Dashboards (Norwegian support)

--- src/components/music/dashboards.tsx
+++ src/components/music/dashboards.tsx
@@ -1,9 +1,27 @@
 'use client'
 
+import {useState, useEffect} from 'react'
 import { setCookie } from '@utils/cookies'
 import Link from 'next/link'
+import dashboard_no from '@text/dashboard/no.json'
+import dashboard_en from '@text/dashboard/en.json'
+import { getCookie } from '@utils/cookies'
+import { language } from '@components/shared/langtoggle/LangToggle'
 
 export default function Dashboards() {
+    const [lang, setLang] = useState('no')
+    const [text, setText] = useState(dashboard_no)
+
+    useEffect(() => {
+        const dashboardText = lang === 'no' ? dashboard_no : dashboard_en
+        setText(dashboardText)
+    }, [lang])
+
+    useEffect(() => {
+        const temp = getCookie('lang')
+        setLang(temp || 'no')
+    }, [language])
+
     const style = 'flex items-center gap-4 p-4 rounded-lg bg-[var(--color-bg-surface)] shadow-none w-full font-semibold'
 
     function handleClick() {
@@ -12,9 +30,9 @@ export default function Dashboards() {
 
     return (
         <div className='flex flex-col md:flex-row gap-4'>
-            <Link href='/music/dashboard/today' onClick={handleClick} className={style}>Dashboard Today</Link>
-            <Link href='/music/dashboard/all' onClick={handleClick} className={style}>Dashboard All Time</Link>
-            <Link href='/music/dashboard/current' onClick={handleClick} className={style}>Currently Listening</Link>
+            <Link href='/music/dashboard/today' onClick={handleClick} className={style}>{text.dashboardToday}</Link>
+            <Link href='/music/dashboard/all' onClick={handleClick} className={style}>{text.dashboardAllTime}</Link>
+            <Link href='/music/dashboard/current' onClick={handleClick} className={style}>{text.currentlyListening}</Link>
         </div>
     )
 }
