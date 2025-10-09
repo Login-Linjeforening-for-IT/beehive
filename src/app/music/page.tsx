6f6d6382aecf9cb27f64322f5c5a9b5002997b30
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-09 18:08:27 +0200

    Added disclaimer

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -5,6 +5,7 @@ import Intro from '@components/music/intro'
 import no from '@text/music/no.json'
 import en from '@text/music/en.json'
 import Dashboards from '@components/music/dashboards'
+import { MessageSquareWarning } from 'lucide-react'
 
 export default async function Music() {
     const lang = ((await cookies()).get('lang')?.value || 'no') as Lang
@@ -14,6 +15,9 @@ export default async function Music() {
     return (
         <div className='page-container'>
             <div className='page-section--normal space-y-4'>
+                <div className='w-full bg-red-500 p-2 rounded-lg'>
+                    <h1 className='flex gap-2 text-[var(--color-text-main)]'><MessageSquareWarning /> Vi opplever for tiden ustabilitet med Musikk APIet grunnet migrering av databasen for å kunne skalere bedre over tid. Problemene er forventet løst i løpet av kvelden.</h1>
+                </div>
                 <Intro text={text} />
                 <MusicClient lang={lang} initialData={data} />
                 <Dashboards lang={lang} />
