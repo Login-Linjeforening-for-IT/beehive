4ac7af38eee0dce8ec86c7fd12c1c41fde699754
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-09 18:21:52 +0200

    v4.6.9: added disclaimer for english

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -12,11 +12,16 @@ export default async function Music() {
     const data = await getActivity()
     const text = (lang === 'no' ? no : en)
 
+    const error = {
+        no: 'Vi opplever for tiden ustabilitet med Musikk APIet grunnet migrering av databasen for å kunne skalere bedre over tid. Problemene er forventet løst i løpet av kvelden.',
+        en: 'We are currently experiencing instability towards the Music API due to migration of the database to scale better over time. The incident is expected to be solved by midnight.'
+    }
+
     return (
         <div className='page-container'>
             <div className='page-section--normal space-y-4'>
                 <div className='w-full bg-red-500 p-2 rounded-lg'>
-                    <h1 className='flex gap-2 text-[var(--color-text-main)]'><MessageSquareWarning /> Vi opplever for tiden ustabilitet med Musikk APIet grunnet migrering av databasen for å kunne skalere bedre over tid. Problemene er forventet løst i løpet av kvelden.</h1>
+                    <h1 className='flex gap-2 text-[var(--color-text-main)]'><MessageSquareWarning /> {lang === 'no' ? error.no : error.en}</h1>
                 </div>
                 <Intro text={text} />
                 <MusicClient lang={lang} initialData={data} />
