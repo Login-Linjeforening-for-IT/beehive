a67094747527569724ba7eec7f88f1e2271b1223
0herm <0herm@protonmail.com>
2025-11-02 13:15:29 +0100

    proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
     const theme = req.cookies.get('theme')?.value || 'dark'
     const lang = req.cookies.get('lang')?.value || 'no'
     const pwned = Math.floor(Math.random() * 23)
