9253c4842b58967982d352a1440b90e16b46b0fa
0herm <0herm@protonmail.com>
2025-11-17 19:35:16 +0100

    fix image preview

--- src/components/albumImages.tsx
+++ src/components/albumImages.tsx
@@ -49,9 +49,8 @@ export default function AlbumImages({ images, albumId, albumNameNo, albumNameEn,
                             <Image
                                 src={`${config.url.CDN_URL}/albums/${albumId}/${image}`}
                                 alt={lang === 'no' ? albumNameNo : albumNameEn}
-                                className='w-full h-full rounded-lg shadow-md hover:opacity-80 transition-opacity'
-                                width={600}
-                                height={400}
+                                className='w-full h-full rounded-lg shadow-md hover:opacity-80 transition-opacity object-cover'
+                                fill={true}
                                 loading='lazy'
                             />
                         </div>
@@ -66,22 +65,21 @@ export default function AlbumImages({ images, albumId, albumNameNo, albumNameEn,
             )}
 
             {currentIndex !== null && (
-                <div className='fixed inset-0 bg-black/50 flex items-center justify-center z-900' onClick={() => setCurrentIndex(null)}>
-                    <div className='relative top-0 md:top-10 max-w-full md:max-w-6xl max-h-full p-4 md:p-20 px-4 md:px-30 rounded-2xl bg-(--color-bg-body) m-4 md:m-10' onClick={(e) => e.stopPropagation()}>
-                        {currentIndex > 0 && (
+                <div className='fixed inset-0 bg-black/80 flex items-center justify-center z-900' onClick={() => setCurrentIndex(null)}>
+                    <div className='relative w-screen sm:w-[70vw] h-auto aspect-3/2 p-2 md:p-20 px-2 md:px-30 rounded-2xl bg-(--color-bg-body) m-4 md:m-10' onClick={(e) => e.stopPropagation()}>
+                        {currentIndex !> 0 && (
                             <button
-                                className='absolute left-5 top-1/2 justify-center z-10 cursor-pointer bg-(--color-bg-body)/90 rounded-full p-1 sm:bg-none'
+                                className='absolute left-5 top-1/2 transform -translate-y-1/2 justify-center z-10 cursor-pointer bg-(--color-bg-body)/90 rounded-full p-1 sm:bg-none'
                                 onClick={(e) => { e.stopPropagation(); goToPrevious() }}
                             >
                                 <ChevronLeft size={30} />
                             </button>
                         )}
                         <Image
-                            src={`${config.url.CDN_URL}/albums/${albumId}/${images[currentIndex]}`}
+                            src={`${config.url.CDN_URL}/albums/${albumId}/${images[currentIndex!]}`}
                             alt={lang === 'no' ? albumNameNo : albumNameEn}
-                            className='max-w-full max-h-full object-contain rounded-2xl'
-                            width={1200}
-                            height={800}
+                            className='w-full h-full object-contain rounded-2xl'
+                            fill={true}
                             priority
                         />
                         <button
@@ -90,9 +88,9 @@ export default function AlbumImages({ images, albumId, albumNameNo, albumNameEn,
                         >
                             <X size={30} />
                         </button>
-                        {currentIndex < images.length - 1 && (
+                        {currentIndex !< images.length - 1 && (
                             <button
-                                className='absolute right-5 top-1/2 justify-center z-10 cursor-pointer bg-(--color-bg-body)/90 rounded-full p-1 sm:bg-none'
+                                className='absolute right-5 top-1/2 transform -translate-y-1/2 justify-center z-10 cursor-pointer bg-(--color-bg-body)/90 rounded-full p-1 sm:bg-none'
                                 onClick={(e) => { e.stopPropagation(); goToNext() }}
                             >
                                 <ChevronRight size={30} />
