3856e0de6fb40a961e0b80cb8732dc08fb868988
eirikhanasand <eirik.hanasand@gmail.com>
2026-04-14 18:50:47 +0200

    v5.6.1: Fixed status ui

--- src/app/ai/[id]/page.tsx
+++ src/app/ai/[id]/page.tsx
@@ -1,16 +1,6 @@
-import { cookies } from 'next/headers'
-// import PageClient from './pageClient'
+import PageClient from './pageClient'
 
 export default async function page({ params }: PromisedPageProps) {
     const id = String((await params).id)
-    const lang = ((await cookies()).get('lang')?.value || 'no') as Lang
-    // return <PageClient id={id} />
-    console.log(id)
-
-    return (
-        <div className='page-container min-h-[calc(100vh-var(--h-topbar))]'>
-            <h1 className='text-xl'>{lang === 'no' ? 'Under oppussing!' : 'Under maintenance!'}</h1>
-            <h1>{lang === 'no' ? 'Kom tilbake senere.' : 'Check back soon!'}</h1>
-        </div>
-    )
+    return <PageClient id={id} />
 }
